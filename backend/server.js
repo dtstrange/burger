96f9eadee6c4891dd47a0cbd66f4267ee13e1001
Dustin Frank <dustintstrange@gmail.com>
2018-01-08 22:33:24 -0800

    moved files into backend and frontend added procfile updated path for files

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser')
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static("frontend"));
 
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(bodyParser.urlencoded({ extended: false }));
